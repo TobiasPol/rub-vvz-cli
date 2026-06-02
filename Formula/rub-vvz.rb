8383ba93f65e10e46a5c6517a33dccb7086c6902
TobiasPol <tobias.polley@posteo.de>
2026-06-02 19:22:13 +0200

    build: update Homebrew formula for v0.1.0-beta.2

--- Formula/rub-vvz.rb
+++ Formula/rub-vvz.rb
@@ -1,8 +1,8 @@
 class RubVvz < Formula
   desc "CLI for the public Ruhr-Universitaet Bochum course catalogue"
   homepage "https://github.com/TobiasPol/rub-vvz-cli"
-  url "https://github.com/TobiasPol/rub-vvz-cli/archive/refs/tags/v0.1.0-beta.1.tar.gz"
-  sha256 "ea99cb9365af74a97015666b1a7d8cd253227bf1e97a0cb78f754c41d10a83ea"
+  url "https://github.com/TobiasPol/rub-vvz-cli/archive/refs/tags/v0.1.0-beta.2.tar.gz"
+  sha256 "c91aef1280b09e090f92d912f9b5e4e8b1bbfba8dc89d147633dc555e004d76e"
   license "MIT"
 
   depends_on "rust" => :build
